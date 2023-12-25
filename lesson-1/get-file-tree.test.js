5896a5607b2dab074b53502b7d86c0118747ec55
Alex Oshchepkov <fastex93@gmail.com>
2023-12-25 21:43:12 +0300

    Поправлен тест модуля get-file-tree

--- lesson-1/get-file-tree.test.js
+++ lesson-1/get-file-tree.test.js
@@ -1,3 +1,4 @@
+const mock = require('mock-fs');
 const treeModule = require('./get-file-tree');
 
 describe('Tree', () => {
@@ -7,22 +8,35 @@ describe('Tree', () => {
     beforeEach(() => {
         getFileTreeSpy = jest.spyOn(treeModule, 'getFileTree');
         consoleLogSpy = jest.spyOn(console, 'log');
+
+        mock({
+            'folder-two': {
+                'some-file.txt': 'file content here',
+                'inner-folders': {
+                    'file1': '',
+                    'file2': '',
+                    'file3': '',
+                }
+            },
+            'test-folder': {},
+        });
     })
 
     afterEach(() => {
         jest.clearAllMocks();
+        mock.restore();
     })
 
     it('should log some of files', async () => {
         await getFileTreeSpy('./', 1);
 
-        expect(consoleLogSpy).toHaveBeenCalledWith('├── package-lock.json');
-        expect(consoleLogSpy).toHaveBeenCalledWith('└── package.json');
+        expect(consoleLogSpy).toHaveBeenCalledWith('├── folder-two');
+        expect(consoleLogSpy).toHaveBeenCalledWith('└── test-folder');
     })
 
     it('should call console.log `n` times', async () => {
         await getFileTreeSpy('./', 2);
 
-        expect(consoleLogSpy).toHaveBeenCalledTimes(216);
+        expect(consoleLogSpy).toHaveBeenCalledTimes(4);
     })
 })
